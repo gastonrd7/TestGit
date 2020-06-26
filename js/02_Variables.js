a5b1707a0772a7138672b91265034db80c08eed1
ruizdiazmartin <ruizdiazmartin@gmail.com>
2020-06-26 15:24:09 -0300

    Se agrega cambios de variables

--- js/02_Variables.js
+++ js/02_Variables.js
@@ -14,4 +14,27 @@ console.log('booleana', booleana);
 var colores = ['rojo', 'amarillo'];
 console.log('colores', colores[0]);
 
-//Hola
+//Variables de Objetos {Object: propiedad y valor}
+var jugo1 = {
+    "ingrediente1": "fresa",
+    "ingrediente2": "naranja",
+    "ingrediente3": "pera"
+}
+
+//Variables de Objetos {Object: propiedad y valor} //Aca los paso con los nobres de las propiedades, pero el comportamiento es el mismo
+var jugo2 = {
+    ingrediente1: "fresa",
+    ingrediente2: "naranja",
+    ingrediente3: "pera"
+}
+console.log("jugo", jugo1.ingrediente1);
+console.log("jugo", jugo2.ingrediente2);
+
+//variables DOM (Modelo de objetos del documento) 
+//Por Id
+var caja = document.querySelector("#caja");
+console.log("caja", caja);
+
+//Por clase
+var cajas = document.querySelector(".cajas");
+console.log("caja", cajas);
