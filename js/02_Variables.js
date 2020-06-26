edf613c5b24cf6e2d61d48328948b14bc36027bc
ruizdiazmartin <ruizdiazmartin@gmail.com>
2020-06-26 14:14:31 -0300

    Se realiza cambios en las variables

---
+++ js/02_Variables.js
@@ -0,0 +1,17 @@
+//variables numericas, se recomineda crear el nombre en camelCase, ya que javascript es case sensitive
+var numero = 5;
+console.log('numero', numero);
+
+//variables de texto
+var texto = 'palabras';
+console.log('texto', texto);
+
+//variables booleanas
+var booleana = true;
+console.log('booleana', booleana);
+
+//variables de tipo array
+var colores = ['rojo', 'amarillo'];
+console.log('colores', colores[0]);
+
+//Hola
