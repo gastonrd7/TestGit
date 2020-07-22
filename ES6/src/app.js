3def11b5d89d06d461024ed3f3f2d0cc4c1037f2
ruizdiazmartin <ruizdiazmartin@gmail.com>
2020-07-22 15:14:44 -0300

    Se sube cambio 22-07-20

---
+++ ES6/src/app.js
@@ -0,0 +1,8 @@
+import { mensajeAlerta, saludo } from './modulos/mensajeAlerta';
+import Persona from './modulos/clasePersona';
+
+mensajeAlerta('Hola Martincho');
+saludo();
+
+const usuario = new Persona('Martin', 40);
+usuario.mostrarInfo();
--- ES6/webpack.config.js
+++ ES6/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './srcs/app.js',
+    entry: './src/app.js',
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, '/'),
@@ -12,5 +12,6 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader"
         }]
-    }
+    },
+    mode: 'development'
 };
